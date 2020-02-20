0fdfc2fde5c286b56f64eb8be4a5d0c81dd81671
msaaddev <mrsaadirfan@gmail.com>
2020-02-20 09:41:05 +0500

    📦 NEW: brute form function

--- Problem 2/main.cpp
+++ Problem 2/main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <ctime>
+#include <random>
 using namespace std;
+
+void RandomArray(int array[], int size);
+int maximumSubArrayBruteForce(int array[]);
 
 int main()
 {
+}
+
+void RandomArray(int array[], int size)
+{
+    for (int i = 0; i < size; i++)
+        array[i] = rand();
+}
+
+int maximumSubArrayBruteForce(int array[], int size)
+{
+    int maxSum = -99999999;
+    for (int i = 0; i < size; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < size; j++)
+        {
+            sum = sum + array[j];
+            if (sum > maxSum)
+            {
+                maxSum = sum;
+            }
+        }
+    }
+    return maxSum;
 }
