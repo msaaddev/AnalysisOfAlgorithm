d9e842accd92982b65e4dd706bedbd0a8fb359be
msaaddev <mrsaadirfan@gmail.com>
2020-02-22 11:54:33 +0500

    📦 NEW: recurisive algo

--- Problem 2/main.cpp
+++ Problem 2/main.cpp
@@ -3,9 +3,18 @@
 #include <random>
 using namespace std;
 
+struct recursiveLowsHighs
+{
+    int low;
+    int high;
+    int sum;
+    int element;
+};
+
 void RandomArray(int array[], int size);
 int maximumSubArrayBruteForce(int array[], int size, int &low, int &high);
-int maximumSubArrayRecursive(int array[]);
+recursiveLowsHighs maximumCrossingSubArray(int array[], int low, int mid, int high);
+recursiveLowsHighs maximumSubArrayRecursive(int array[], int low, int high);
 
 int main()
 {
@@ -37,6 +46,68 @@ int maximumSubArrayBruteForce(int array[], int size, int &low, int &high)
     return maxSum;
 }
 
-int maximumSubArrayRecursive(int array[], int size, int &low, int &high)
+recursiveLowsHighs maximumCrossingSubArray(int array[], int low, int mid, int high)
 {
+    recursiveLowsHighs position;
+    int leftSum = -999999999;
+    int sum = 0;
+    for (int i = mid; i >= low; i--)
+    {
+        sum = sum + array[i];
+        if (sum > leftSum)
+        {
+            leftSum = sum;
+            position.low = i;
+        }
+    }
+
+    int rightSum = -99999999;
+    sum = 0;
+    for (int i = mid + 1; i <= high; i++)
+    {
+        sum = sum + array[i];
+        if (sum > rightSum)
+        {
+            rightSum = sum;
+            position.high = i;
+        }
+    }
+
+    position.sum = leftSum + rightSum;
+    return position;
+}
+
+recursiveLowsHighs maximumSubArrayRecursive(int array[], int low, int high)
+{
+    recursiveLowsHighs position1, position2, position3, result;
+
+    if (high == low)
+    {
+        result.low = low;
+        result.high = high;
+        result.element = array[low];
+        return result;
+    }
+    else
+    {
+        int mid = (low + high) / 2;
+        mid = floor(mid);
+
+        position1 = maximumSubArrayRecursive(array, low, mid);
+        position2 = maximumSubArrayRecursive(array, mid + 1, high);
+        position3 = maximumCrossingSubArray(array, low, mid, high);
+
+        if ((position1.sum >= position2.sum) && (position1.sum >= position3.sum))
+        {
+            return position1;
+        }
+        else if ((position2.sum >= position1.sum) && (position2.sum >= position3.sum))
+        {
+            return position2;
+        }
+        else
+        {
+            return position3;
+        }
+    }
 }
