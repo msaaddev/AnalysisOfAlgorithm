422544b3e5f490f4c57b78e71431d4b2c99a52c4
msaaddev <mrsaadirfan@gmail.com>
2020-03-11 15:53:16 +0500

    📦NEW: dynamic approach

--- dynamic/main.cpp
+++ dynamic/main.cpp
@@ -17,8 +17,31 @@ int cutRod(int n, int p[])
     int q = -999999999;
     for (int i = 0; i < n; i++)
     {
-        if (q < (p[i] + cutRod(n - 1, p)))
-            q = p[i] + cutRod(n - 1, p);
-    }
+/*
+    Cut Rod Dynamic Approach
+*/
+int memoizedCutRod(int p[], int n)
+{
+
+    int *r = new int[n];
+    for (int i = 0; i < n; i++)
+        r[i] = -999999999;
+    return memoizedCutRodAux(p, n, r);
+}
+
+int memoizedCutRodAux(int p[], int n, int r[])
+{
+    int q = -999999999;
+
+    if (r[n] >= 0)
+        return r[n];
+    if (n == 0)
+        q = 0;
+    else if (q = -999999999)
+        for (int i = 0; i < n; i++)
+        {
+            if (q < (p[i] + memoizedCutRodAux(p, n - i, r)))
+                q = p[i] + memoizedCutRodAux(p, n - i, r);
+        }
     return q;
 }
