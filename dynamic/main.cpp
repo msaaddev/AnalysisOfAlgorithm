5c27283c8edb7d4fb9181e5b99afe7531c338975
msaaddev <mrsaadirfan@gmail.com>
2020-03-11 15:45:19 +0500

    📦NEW: recursive function

--- dynamic/main.cpp
+++ dynamic/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
+int count = 0;
+int cutRod(int n, int p[]);
 int main()
 {
+}
+
+/*
+    Cut Rod recurssive function
+*/
+int cutRod(int n, int p[])
+{
+    if (n == 0)
+        return 0;
+    int q = -999999999;
+    for (int i = 0; i < n; i++)
+    {
+        if (q < (p[i] + cutRod(n - 1, p)))
+            q = p[i] + cutRod(n - 1, p);
+    }
+    return q;
 }
