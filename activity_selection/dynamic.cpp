2437516332798c8219bb56a65322c1e372e67463
msaaddev <mrsaadirfan@gmail.com>
2020-06-30 19:21:47 +0500

    📖 DOC: comments

--- activity_selection/dynamic.cpp
+++ activity_selection/dynamic.cpp
@@ -11,18 +11,49 @@
 
 #include <iostream>
 #include <string>
-#include <cmath>
 using namespace std;
 
-void dynamicActivitySelector(int s, int f[], int n);
+/* prototypes of functions */
+
+void dynamicActivitySelector(int s[], int f[], int n);
 void printActivities(int **c, int **act, int i, int j);
 
 int main()
 {
+	/* variable declarations */
+	int numOfActivities;
+	cout << "Enter total number of activities: ";
+	cin >> numOfActivities;
+	int *s = new int[numOfActivities];
+	int *f = new int[numOfActivities];
+
+	/* getting input values from user */
+
+	for (int i = 0; i < numOfActivities; i++)
+	{
+		cout << "\nEnter start time of Activity " << i + 1 << " : ";
+		cin >> s[i];
+
+		cout << "Enter finish time of Activity " << i + 1 << " : ";
+		cin >> f[i];
+	}
+
+	/* executing activity selection using dynamic approach */
+
+	dynamicActivitySelector(s, f, numOfActivities);
 }
+
+/**
+ *
+ *
+ * @param {s} – integer array that stores start time of an activity
+ * @param {f} - integer array that stores finish time of an activity
+ * @param {n} - integer that stores total number of activities
+ */
 
 void dynamicActivitySelector(int s[], int f[], int n)
 {
+	/* variable & 2D dynamic array declaration */
 	int j, k;
 	int **c = new int *[n + 2];
 	for (int i = 0; i < n + 2; i++)
@@ -38,11 +69,12 @@ void dynamicActivitySelector(int s[], int f[], int n)
 		c[i][i + 1] = 0;
 	}
 
-	c[n + 2][n + 2] = 0;
+	c[n + 1][n + 1] = 0;
 
-	for (int l = 2; l < n + 2; l++)
+	/* calculating scheduled activities */
+	for (int l = 2; l < n + 1; l++)
 	{
-		for (int i = 0; i < n - l + 1; i++)
+		for (int i = 0; i < n - l + 2; i++)
 		{
 			j = i + l;
 			c[i][j] = 0;
@@ -58,11 +90,20 @@ void dynamicActivitySelector(int s[], int f[], int n)
 			}
 		}
 	}
-	cout << "A maximum size set of mutually compatible activities has size: " << c[0][n + 2];
+	cout << "A maximum size set of mutually compatible activities has size: " << c[0][n + 1];
 	cout << "The set contains ";
 
-	printActivities(c, act, 0, n + 2);
+	printActivities(c, act, 0, n + 1);
 }
+
+/**
+ *
+ *
+ * @param {c} – 2d integer array
+ * @param {act} - 2d integer array containing the activites
+ * @param {i} - starting index
+ * @param {j} - ending index
+ */
 
 void printActivities(int **c, int **act, int i, int j)
 {
